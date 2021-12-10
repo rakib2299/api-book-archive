9a269f133094a86ca46aea09039a456237a225a3
rakib2299 <zannatul.rakib2299@gmail.com>
2021-12-11 01:03:52 +0600

    html part test

--- js/books.js
+++ js/books.js
@@ -1,1 +1,17 @@
-console.log("object");
+// connect api
+const searchBooks = () => {
+  const searchInput = document.getElementById("search-input");
+  const searchText = searchInput.value;
+  //   console.log(searchText);
+  searchInput.value = "";
+
+  const url = `https://openlibrary.org/search.json?q=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displaySearchBooks(data.docs));
+};
+
+// showing display results
+const displaySearchBooks = (books) => {
+  console.log(books);
+};

7b2dcf6b3836134205827900f930f1c99df56a38
rakib2299 <zannatul.rakib2299@gmail.com>
2021-12-11 16:26:52 +0600

    last update

--- js/books.js
+++ js/books.js
@@ -2,7 +2,7 @@
 const searchBooks = () => {
   const searchInput = document.getElementById("search-input");
   const searchText = searchInput.value;
-  //   console.log(searchText);
+  //   clear input field
   searchInput.value = "";
 
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
@@ -13,5 +13,31 @@ const searchBooks = () => {
 
 // showing display results
 const displaySearchBooks = (books) => {
-  console.log(books);
+  // console.log(books);
+  const booksCard = document.getElementById("books-card");
+  booksCard.textContent = "";
+  if (books.length === 0) {
+    const h3 = document.createElement("h3");
+    h3.innerText = "No Results Found!";
+    booksCard.appendChild(h3);
+  } else {
+    books.forEach((book) => {
+      console.log(book);
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+        <div class="card">
+          <img src='https://covers.openlibrary.org/b/id/${
+            book.cover_i
+          }-M.jpg' class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h4 class="card-title">${book.title}</h4>
+                <h5>Author: ${book.author_name.slice(0, 25)}</h5>
+                <h6>First Publish Date: ${book.first_publish_year}</h6>
+           </div>
+         </div>
+      `;
+      booksCard.appendChild(div);
+    });
+  }
 };
